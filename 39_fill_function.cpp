503bb6e06a393f041b8c4f25b39be99ac4d58d81
Trevor <trevor.ruttan05@gmail.com>
2023-09-10 21:45:54 -0700

    modified:   36_pass_array_to_function.cpp 	new file:   36_pass_array_to_function.exe 	new file:   37_search_an_array.cpp 	new file:   37_search_an_array.exe 	new file:   38_bubblesort.cpp 	new file:   38_bubblesort.exe 	new file:   39_fill_function.cpp 	new file:   39_fill_function.exe

---
+++ 39_fill_function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    // fill() = fills a range of elements with a specified value
+    //          fill(begin, end, value)
+
+    const int SIZE = 99;
+    std::string foods[SIZE];
+
+    fill(foods, foods + (SIZE/3), "pizza");
+    fill(foods+ (SIZE/3), foods + (SIZE/3)*2, "burger");
+    fill(foods+ (SIZE/3)*2, foods + SIZE, "hotdog");
+
+    for(std::string food : foods){
+        std::cout << food << ' ';
+    }
+
+    return 0;
+}
